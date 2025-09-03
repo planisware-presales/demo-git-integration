c28a31276d3dda8a9fdd4682dd0ccf4c1b34f4a8
oguimard-plw <olivier.guimard@planisware.com>
2025-09-03 10:42:57 +0200

    Create second_file.py
    adding a script to share the color of my shirt

---
+++ second_file.py
@@ -0,0 +1,6 @@
+my_shirt_color_today = ["red"]
+
+if my_shirt_color_today == ["red"]:
+    print("Hello friend, I like the color of your shirt! Red is my favorite")
+else:
+    print("Hello friend! Why didn't you wear your red shirt today?")
